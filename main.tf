54ead266b805992382af02de940a0349768b67df
317mahanandi <101698754+317mahanandi@users.noreply.github.com>
2023-07-31 12:56:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "finance" {
+bucket = "finanace-21092020"
+tags = {
+Description = "Finance and Payroll"
+}
+}
+resource "aws_s3_bucket_object" "finance-2020" {
+content = "/root/finance/finance-2020.doc"
+key = "finance-2020.doc"
+bucket = aws_s3_bucket.finance.id
+}
+data "aws_iam_group" "finance-data" {
+group_name = "finance-analysts"
+}
